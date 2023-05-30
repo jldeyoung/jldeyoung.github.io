f3b334c47044ae96106865bd331ada16bf687b07
Juni DeYoung <jldeyoung@willamette.edu>
2023-05-30 12:05:39 -0700

    header/footer shenanigans

--- Common/hdr-ftr.js
+++ Common/hdr-ftr.js
@@ -1,15 +1,30 @@
 let appHeader = `
     <!-- Navbar -->
 	<div class="w3-top" style="font-family: Lato">
+	  <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
+      <i class="fa fa-bars"></i>
+    </a>
 	  <div class="w3-bar w3-theme w3-top w3-left-align w3-large">
 		<a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
-		<a href="/index.html#home" class="w3-bar-item w3-button">HOME</a>
+		<a href="/index.html#home" class="w3-bar-item w3-button"> HOME</a>
 		<a href="/index.html#about" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> ABOUT</a>
+		<a href="/index.html#portfolio" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-folder"></i> PORTFOLIO</a>
 		<a href="/index.html#contact" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-envelope"></i> CONTACT</a>
 		<a href="/resume.html" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> RESUME</a>
 		<a href="/Blog/index.html" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-th"></i> BLOG</a>
 	  </div>
 	</div>
+	
+	<!-- Navbar on small screens -->
+	  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
+		<a href="/index.html#home" class="w3-bar-item w3-button"> HOME</a>
+		<a href="/index.html#about" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT</a>
+		<a href="/index.html#portfolio" class="w3-bar-item w3-button" onclick="toggleFunction()">PORTFOLIO</a>
+		<a href="/index.html#contact" class="w3-bar-item w3-button" onclick="toggleFunction()">CONTACT</a>
+		<a href="/resume.html" class="w3-bar-item w3-button" onclick="toggleFunction()">RESUME</a>
+		<a href="/Blog/index.html" class="w3-bar-item w3-button" onclick="toggleFunction()">BLOG</a>
+		<!--<a href="#" class="w3-bar-item w3-button">SEARCH</a>-->
+	  </div>
 `;
 
 let appFooter = `
@@ -22,10 +37,21 @@ let appFooter = `
 		<a href="mailto:jldeyoung@willamette.edu"><i class="fa fa-envelope w3-hover-opacity" target="_blank"></i></a>
 	  </div>
 	  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
-	  <p>Images by Juni L DeYoung &copy; 2021.</p>
+	  <p>Images by Juni L DeYoung &copy; 2022.</p>
 	</footer>
 `;
 
+let imports = `
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&family=EB+Garamond">
+	<script src="https://kit.fontawesome.com/2305782426.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="Common/homepage.css">
+`;
+
+document.getElementById("imports").innerHTML += imports;
 document.getElementById("app-header").innerHTML = appHeader;
 document.getElementById("app-footer").innerHTML = appFooter;
 
