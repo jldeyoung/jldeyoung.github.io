0f2f952a3694eb815d8f39eb2cb0555a1ff25f3d
Juni DeYoung <jldeyoung@willamette.edu>
2023-07-13 16:31:46 -0700

    various formatting and content fixes

--- Common/script.js
+++ Common/script.js
@@ -24,24 +24,3 @@ function loadMore() {
 
 //document.getElementById("app-header").innerHTML = appHeader;
 //document.getElementById("app-footer").innerHTML = appFooter;
-
-// Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
-
-function hide_show(id) {
-  alert(`hide_show(${id})`);
-  var x = document.getElementById(id);
-  if (x.className.indexOf("w3-show") == -1) {
-	x.className += " w3-show";
-	x.previousElementSibling.style.overflow = "visible";
-  } else {
-	x.className = x.className.replace(" w3-show", "");
-  }
-}
