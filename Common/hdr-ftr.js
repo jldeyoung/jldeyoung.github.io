78551993e06c05a818c3a9542374bbdb51a2cec7
wu-jldeyoung <97631463+wu-jldeyoung@users.noreply.github.com>
2024-08-17 23:29:55 -0400

    reduce image dimensions, add new headshot, minor content updates

--- Common/hdr-ftr.js
+++ Common/hdr-ftr.js
@@ -37,7 +37,7 @@ let appFooter = `
 			<a href="mailto:jldeyoung@live.com"><i class="fa fa-envelope w3-hover-opacity" target="_blank"></i></a>
 		  </div>
 	  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
-	  <p>Images by Juni L DeYoung &copy; 2023.</p>
+	  <p>Images by Juni L DeYoung &copy; 2024.</p>
 	</div>
 `;
 
